f27e944c8a8af1114f057fc6e654a09fff184d2d
Sebastian Estrada <est21405@uvg.edu.gt>
2025-06-10 20:01:09 -0600

    Correcciones

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,32 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { Routes, Route } from 'react-router-dom'
-import Config from "./assets/Constants/Constant.jsx"
+import { Routes, Route } from 'react-router-dom';
+import Config from "./assets/Constants/Constant.jsx";
 
-import Dashboard from './Pages/Dashboard/Dashboard.jsx'
-import Empresa from './Pages/Empresas/Empresa.jsx'
-import Colaboradores from './Pages/Colaboradores/Colaboradores.jsx'
-import Localidades from './Pages/Localidades/Localidades.jsx'
+import Dashboard from './Pages/Dashboard/Dashboard.jsx';
+import Empresa from './Pages/Empresas/Empresa.jsx';
+import Colaboradores from './Pages/Colaboradores/Colaboradores.jsx';
+import Localidades from './Pages/Localidades/Localidades.jsx';
 
-import Header from './Components/Header/Header.jsx'
+import Header from './Components/Header/Header.jsx';
 
 function App() {
-
   return (
-    <div className='App'>
+    <div className="App">
       <Header />
-      <Routes>
-        <Route>
+      <main className="MainContent">
+        <Routes>
           <Route path={Config.Dashboard.path} element={<Dashboard />} />
           <Route path={Config.Empresas.path} element={<Empresa />} />
           <Route path={Config.Colaboradores.path} element={<Colaboradores />} />
           <Route path={Config.Localidades.path} element={<Localidades />} />
-        </Route>
-      </Routes>
+        </Routes>
+      </main>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
